bc61a7bd810fae602f5295906fb5cb06eccbf32a
Ilya <i1ya1l@yandex.ru>
2020-09-24 14:25:05 +0300

    update test

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -4,33 +4,34 @@ import gendiff from '../src';
 
 const extNames = ['json', 'yml', 'ini'];
 
-const getExpectedFile = (name) => fs.readFileSync(path.join(`__test__/__fixtures__/${name}`), 'utf-8');
+let resultStylish;
+let resultPlain;
+let resultJson;
+
+beforeAll(() => {
+  resultStylish = fs.readFileSync(path.join('__test__/__fixtures__/stylish'), 'utf-8');
+  resultPlain = fs.readFileSync(path.join('__test__/__fixtures__/plain'), 'utf-8');
+  resultJson = fs.readFileSync(path.join('__test__/__fixtures__/json'), 'utf-8');
+});
 
 extNames.forEach((ext) => {
   const pathFile1 = `__test__/__fixtures__/before.${ext}`;
   const pathFile2 = `__test__/__fixtures__/after.${ext}`;
   describe('test stylish', () => {
     test(`diff stylish ${ext}`, () => {
-      const expectedFile = getExpectedFile('stylish');
-      const result = gendiff(pathFile1, pathFile2, 'stylish');
-      expect(result).toBe(expectedFile);
+      expect(gendiff(pathFile1, pathFile2, 'stylish')).toBe(resultStylish);
     });
   });
 
   describe('test plain', () => {
     test(`diff stylish ${ext}`, () => {
-      const expectedFile = getExpectedFile('plain');
-      const result = gendiff(pathFile1, pathFile2, 'plain');
-      expect(result).toBe(expectedFile);
+      expect(gendiff(pathFile1, pathFile2, 'plain')).toBe(resultPlain);
     });
   });
 
   describe('test json', () => {
     test(`diff stylish ${ext}`, () => {
-      const expectedFile = getExpectedFile('json');
-      const result = gendiff(pathFile1, pathFile2, 'json');
-
-      expect(result).toBe(expectedFile);
+      expect(gendiff(pathFile1, pathFile2, 'json')).toBe(resultJson);
     });
   });
 });
